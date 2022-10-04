8c84e5c98e4d0657f38131483df94405d813b10a
Hector-rob <A01634105@itesm.mx>
2022-10-04 13:58:59 -0500

    Rebase

--- app/src/main/java/com/application/app/modules/seleccinalimentos/data/model/SelecciNAlimentosModel.kt
+++ app/src/main/java/com/application/app/modules/seleccinalimentos/data/model/SelecciNAlimentosModel.kt
@@ -8,7 +8,10 @@ data class SelecciNAlimentosModel(
   /**
    * TODO Replace with dynamic value
    */
-  var txtH5: String? = MyApp.getInstance().resources.getString(R.string.lbl_fruta_y_verdura2)
+  var fruta: String? = MyApp.getInstance().resources.getString(R.string.lbl_fruta_y_verdura2),
+  var canasta: String? = MyApp.getInstance().resources.getString(R.string.lbl_canasta_basica),
+  var embutidos: String? = MyApp.getInstance().resources.getString(R.string.lbl_embutidos_y_lacteos),
+  var abarrotes: String? = MyApp.getInstance().resources.getString(R.string.lbl_abarrotes)
   ,
   /**
    * TODO Replace with dynamic value
